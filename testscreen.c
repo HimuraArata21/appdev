d9a14d849c444aaf2c8d1ccf6f8c82a2d9ef622a
1900306 <e1900306@shell.vamk.fi>
2020-04-16 14:05:03 +0300

    Sound module added

--- testscreen.c
+++ testscreen.c
@@ -2,10 +2,11 @@
 #include "screen.h"
 #include <unistd.h>
 #include "comm.h"
+#include "sound.h"
 
 int main(void)
 {
-    Position cur = getscreensize();
+/*    Position cur = getscreensize();
     char poststr[100];
     sprintf(poststr, "row=%d&col=%d", cur.row, cur.col);
     senddata(poststr, URL);
@@ -13,4 +14,11 @@ int main(void)
     printf("Screen size,ROW-%d, COL-%d\n", cur.row, cur.col);
     printf("HTTP post is sent to %s\n", URL);
     getchar();
+*/
+
+FILE *fp;
+fp = fopen("test.wav", "r");
+WAVheader h = readwavhdr(fp);
+fclose(fp);
+displayWAVhdr(h);
 }
